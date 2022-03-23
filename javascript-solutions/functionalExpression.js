d6c4327c6aa21e88d7e785d50a3ce3ead6533c6e
Ghstfc <maks-merkulov-2017@mail.ru>
2022-03-23 14:04:49 +0300

    added PI

---
+++ javascript-solutions/functionalExpression.js
@@ -0,0 +1,19 @@
+"use strict";
+let variable = f => (...args) => {
+    switch (f) {
+        case 'x':
+            return args[0];
+        case 'y':
+            return args[1];
+        case 'z':
+            return args[2];
+    }
+}
+let cnst = f => (...args) => f;
+let add = (f1, f2) => (...args) => f1(...args) + f2(...args);
+let subtract = (f1, f2) => (...args) => f1(...args) - f2(...args);
+let divide = (f1, f2) => (...args) => f1(...args) / f2(...args);
+let multiply = (f1, f2) => (...args) => f1(...args) * f2(...args);
+let negate = f => (...args) => -f(...args);
+let pi = cnst(Math.PI);
+let e = cnst(Math.E);
