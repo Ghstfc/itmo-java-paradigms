c59c0ed3c7250005772b1c3ab74b495348f8dd7e
Ghstfc <maks-merkulov-2017@mail.ru>
2022-04-06 13:39:51 +0300

    biba

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -1,15 +1,10 @@
 "use strict";
-let ExprPrototype = {
-
-    evaluate: function (x, y, z) {
-
-    },
-    toString: function () {
-
-    }
-}
 
 function Variable(value) {
+
+    if (value !== 'x' && value !== 'y' && value !== 'z') {
+        throw new CustomError("Wrong variable");
+    }
     return {
         value: value,
         evaluate: function (x, y, z) {
@@ -23,114 +18,112 @@ function Variable(value) {
             }
         },
         toString: function () {
-            return this.value;
-        },
-        parse: function () {
-            return this.value;
-        }
-    }
-}
-
-function Const(value) {
-    return {
-        value: value,
-        evaluate: function () {
-            return Number(this.value);
-        },
-        toString: function () {
             return String(this.value);
-        }
-    }
-}
+        },
+        prefix: function () {
+            return String(this.value);
+        },
 
-function Negate(value) {
-    return {
-        value: value,
-        evaluate: function (x, y, z) {
-            return -(this.value.evaluate(x, y, z));
-        },
-        toString: function () {
-            return this.value + " negate";
-        },
     }
-}
 
-function Sinh(value) {
-    return {
-        value: value,
-        evaluate: function (x, y, z) {
-            return Math.sinh(this.value.evaluate(x, y, z));
-        },
-        toString: function () {
-            return String(value) + " sinh";
-        }
-    }
 }
 
 
-function Cosh(value) {
-    return {
-        value: value,
-        evaluate: function (x, y, z) {
-            return Math.cosh(this.value.evaluate(x, y, z));
-        },
-        toString: function () {
-            return String(value) + " cosh";
-        }
-    }
-}
-
-function Add(v1, v2) {
+function BinaryOperation(v1, v2, sign, op) {
     return {
         v1: v1,
         v2: v2,
+        sign: sign,
+        op: op,
         evaluate: function (x, y, z) {
-            return v1.evaluate(x, y, z) + v2.evaluate(x, y, z);
+            return this.op(this.v1.evaluate(x, y, z), this.v2.evaluate(x, y, z));
         },
         toString: function () {
-            return v1.toString() + " " + v2.toString() + " +";
+            return this.v1.toString() + " " + this.v2.toString() + " " + this.sign;
+        },
+        prefix: function () {
+            return "(" + this.sign + " " + this.v1.prefix() + " " + this.v2.prefix() + ')';
         }
     }
+}
+
+
+function Add(v1, v2) {
+    return BinaryOperation(v1, v2, "+", (a, b) => a + b)
 }
 
 function Subtract(v1, v2) {
-    return {
-        v1: v1,
-        v2: v2,
-        evaluate: function (x, y, z) {
-            return v1.evaluate(x, y, z) - v2.evaluate(x, y, z);
-        },
-        toString: function () {
-            return v1.toString() + " " + v2.toString() + " -";
-        }
-    }
-}
-
-function Multiply(v1, v2) {
-    return {
-        v1: v1,
-        v2: v2,
-        evaluate: function (x, y, z) {
-            return v1.evaluate(x, y, z) * v2.evaluate(x, y, z);
-        },
-        toString: function () {
-            return v1.toString() + " " + v2.toString() + " *";
-        }
-    }
+    return BinaryOperation(v1, v2, "-", (a, b) => a - b)
 }
 
 function Divide(v1, v2) {
+    return BinaryOperation(v1, v2, "/", (a, b) => a / b)
+}
+
+function Multiply(v1, v2) {
+    return BinaryOperation(v1, v2, "*", (a, b) => a * b)
+}
+
+
+function UnaryOperation(v, sign) {
     return {
-        v1: v1,
-        v2: v2,
+        v: v,
+        sign: sign,
         evaluate: function (x, y, z) {
-            return v1.evaluate(x, y, z) / v2.evaluate(x, y, z);
+            switch (sign) {
+                case '+':
+                    return Number(v);
+                case '-':
+                    return -Number(v.evaluate(x, y, z));
+                case ' sinh':
+                    return Math.sinh(v.evaluate(x, y, z));
+                case ' cosh':
+                    return Math.cosh(v.evaluate(x, y, z));
+            }
         },
         toString: function () {
-            return v1.toString() + " " + v2.toString() + " /";
+            switch (sign) {
+                case '+':
+                    return String(v);
+                case '-':
+                    return String(v) + ' negate'
+                case ' sinh':
+                    return String(v) + sign;
+                case ' cosh':
+                    return String(v) + sign;
+            }
+        },
+        prefix: function () {
+            switch (sign) {
+                case '+':
+                    return String(v);
+                case '-':
+                    return '(negate ' + String(v.prefix()) + ')';
+                case ' sinh':
+                    return sign.trim() + " " + String(v);
+                case ' cosh':
+                    return sign.trim() + " " + String(v);
+            }
         }
     }
 }
+
+function Const(v) {
+    return UnaryOperation(v, '+');
+}
+
+function Negate(v) {
+    return UnaryOperation(v, '-')
+}
+
+function Sinh(v) {
+    return UnaryOperation(v, ' sinh');
+}
+
+function Cosh(v) {
+    return UnaryOperation(v, ' cosh');
+}
+
 
 function parse(str) {
     let perm = String(str).trim().split(" ");
@@ -174,4 +167,105 @@ function parse(str) {
         }
     }
     return stack.pop();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function CustomError(message) {
+    Error.call(this, message);
+    this.message = message;
+}
+
+CustomError.prototype = Object.create(Error.prototype);
+CustomError.prototype.name = "CustomError";
+
+function parsePrefix(str) {
+    check(str);
+    str = str.replaceAll("(", " ");
+    str = str.replaceAll(")", " ");
+    let perm = String(str).trim().split(" ");
+    perm = perm.filter((n) => n !== "");
+    let stack = [];
+    for (let i = perm.length - 1; i >= 0; i--) {
+        if (!isNaN(Number(perm[i]))) {
+            stack.push(new Const(perm[i]));
+            continue;
+        }
+        switch (perm[i]) {
+            case 'x':
+            case 'y':
+            case 'z':
+                stack.push(new Variable(perm[i]));
+                break;
+            case '*':
+                if (stack.length === 1)
+                    throw new CustomError("Bruh");
+                stack.push(new Multiply(stack.pop(), stack.pop()));
+                break;
+            case '/':
+                if (stack.length === 1)
+                    throw new CustomError("Bruh");
+                stack.push(new Divide(stack.pop(), stack.pop()));
+                break;
+            case '+':
+                if (stack.length === 1)
+                    throw new CustomError("Bruh");
+                stack.push(new Add(stack.pop(), stack.pop()));
+                break;
+            case '-':
+                if (stack.length === 1)
+                    throw new CustomError("Bruh");
+                stack.push(new Subtract(stack.pop(), stack.pop()));
+                break;
+            case'negate':
+                stack.push(new Negate(stack.pop()));
+                break
+            case'sinh' :
+                stack.push(new Sinh(stack.pop()));
+                break;
+            case'cosh':
+                stack.push(new Cosh(stack.pop()));
+                break;
+            default:
+                throw new CustomError("wrong token");
+        }
+    }
+    if (stack.length !== 1)
+        throw new CustomError("wrong expr");
+    return stack.pop();
+}
+
+function check(s) {
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(')
+            count++;
+        else if (s[i] === ')')
+            count--;
+        if (count < 0) {
+            throw new CustomError("Wrong bracket sequence");
+        }
+    }
+    if (count > 0)
+        throw new CustomError("Wrong bracket sequence");
+    return true;
 }
