bbd1617ef846f970bbe62462078e80e0f67c8277
Ghstfc <maks-merkulov-2017@mail.ru>
2022-04-27 13:49:33 +0300

    added modification of expression.clj

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -6,14 +6,10 @@
 (defn variable [e] (fn [x] (get x e)))
 (defn constant [e] (fn [x] e))
 (defn negate [e] (fn [x] (- (e x))))
-(defn sinh [e] (fn [x] (Math/sinh (e x))))
-(defn cosh [e] (fn [x] (Math/cosh (e x))))
-
-
-
-(def bin {'+ add '- subtract '* multiply '/ divide 'negate negate})
-(def uno {'sinh sinh 'cosh cosh 'negate negate})
-
+(defn pow [e1 e2] (fn [x] (Math/pow (e1 x) (e2 x))))
+(defn log [e1 e2] (fn [x] (/ (Math/log (Math/abs (e2 x))) (Math/log (Math/abs (e1 x))))))
+(def bin {'+ add '- subtract '* multiply '/ divide 'negate negate 'pow pow 'log log})
+(def uno {'negate negate})
 (defn parse [s]
   (cond
     (number? s) (constant s)
@@ -21,10 +17,6 @@
     (= 2 (count s)) ((get uno (nth s 0)) (parse (nth s 1)))
     :else ((get bin (nth s 0)) (parse (nth s 1)) (parse (nth s 2)))
     ))
-
-
-
-
 (defn parseFunction [s] (parse (read-string s)))
 
 
